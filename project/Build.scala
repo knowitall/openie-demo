11e117fb61243ca3b59d2514f935436dd98f4b1f
Michael Schmitz <michael@schmitztech.com>
2012-05-08 12:10:34 -0700

    Final mock up.

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "edu.washington.cs.knowitall" % "openiedemo-backend" % "0.0.1-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
+      resolvers += (
+        "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"
+      )
     )
 
 }
