e36eaac67f924604b361826a9e1d0f302ff2d8d8
Michael Schmitz <michael@schmitztech.com>
2012-05-11 15:11:00 -0700

    Handle linked entities.

--- app/models/Group.scala
+++ app/models/Group.scala
@@ -5,6 +5,7 @@ import java.util.regex.Pattern
 import scala.collection.JavaConverters._
 
 import edu.washington.cs.knowitall.collection.immutable.Interval
+import edu.washington.cs.knowitall.common.enrich.Traversables._
 import edu.washington.cs.knowitall.commonlib.Range
 
 import edu.washington.cs.knowitall.browser.lucene.ExtractionGroupFetcher
@@ -15,11 +16,16 @@ import edu.washington.cs.knowitall.browser.extraction.FreeBaseEntity
 import edu.washington.cs.knowitall.browser.extraction.FreeBaseType
 
 @SerialVersionUID(42L)
-case class GroupTitlePart(lemma: String, synonyms: Seq[String], entity: Option[FreeBaseEntity], types: Seq[FreeBaseType])
+case class GroupTitlePart(lemma: String, synonyms: Seq[String], entity: Option[FreeBaseEntity], types: Seq[FreeBaseType]) {
+  def text = entity match {
+    case Some(entity) => entity.name
+    case None => lemma
+  }
+}
 
 @SerialVersionUID(43L)
 case class GroupTitle(connector: String, parts: Seq[GroupTitlePart]) {
-  def text: String = parts.iterator.map(_.lemma).mkString(connector)
+  def text: String = parts.iterator.map(_.text).mkString(connector)
 }
 
 @SerialVersionUID(44L)
@@ -37,13 +43,37 @@ object Group {
       Interval.open(range.getStart, range.getEnd)
     }
 
-    val groups = (reg map (reg => (group(reg), reg)) groupBy { case (title, reg) => title.text }).toList.
+    val groups = (reg map (reg => (group(reg), reg)) groupBy { case (title, reg) => title.parts.map(_.lemma) }).toList.
       sortBy { case (text, list) => -list.iterator.map { case (title, reg) => reg.instances.size }.sum }
 
     val collapsed: Seq[(GroupTitle, Iterable[ExtractionGroup[ReVerbExtraction]])] = groups.map { case (text, list) =>
       val (headTitle, _) = list.head
       val titles = list.map { case (title, reg) => title }
-      val title = GroupTitle(headTitle.connector, headTitle.parts)
+
+      // safe because of our groupBy
+      val length = headTitle.parts.length
+      var synonyms: Array[Seq[String]] = Array.fill(length)(Seq.empty)
+      var entities: Array[Option[FreeBaseEntity]] = Array.fill(length)(None)
+      var types: Array[Seq[FreeBaseType]] = Array.fill(length)(Seq.empty)
+      val parts = for (i <- 0 until length) yield {
+        val synonyms: Seq[String] =
+          list.flatMap(_._1.parts(i).synonyms)(scala.collection.breakOut)
+
+        val entities: Option[FreeBaseEntity] =
+          list.flatMap(_._1.parts(i).entity).headOption
+
+        val types: Seq[FreeBaseType] =
+          list.flatMap(_._1.parts(i).types)(scala.collection.breakOut)
+
+        val sortedUniqueSynonyms =
+          synonyms.groupBy(_.toLowerCase).toList.map { case (name, synonyms) =>
+            (synonyms.head, synonyms.size)
+          }.sortBy(- _._2).map(_._1)
+
+        GroupTitlePart(sortedUniqueSynonyms.headOption.getOrElse(headTitle.parts(i).lemma), sortedUniqueSynonyms, entities, types)
+      }
+
+      val title = GroupTitle(headTitle.connector, parts)
       (title, list.map(_._2))
     }
 
@@ -56,12 +86,12 @@ object Group {
             instance.extraction.source.getArgument1.getRange,
             instance.extraction.source.getRelation.getRange,
             instance.extraction.source.getArgument2.getRange)
-        Content(sentence.asScala.toList, url, ranges map rangeToInterval)
+        Content(sentence.asScala.toList.map(Query.clean), url, ranges map rangeToInterval)
       }.toList
 
       Group(title, list)
     }.filter(_.contents.size > 0)
-    
+
     .sortBy(-_.contents.size)
   }
 }
--- app/models/Query.scala
+++ app/models/Query.scala
@@ -1,21 +1,15 @@
 package models
 
 import edu.washington.cs.knowitall.browser.extraction.Instance
+import edu.washington.cs.knowitall.common.Resource.using
 import edu.washington.cs.knowitall.browser.lucene.ParallelExtractionGroupFetcher
 import edu.washington.cs.knowitall.browser.extraction.ExtractionGroup
 import edu.washington.cs.knowitall.browser.extraction.ReVerbExtraction
-
 import java.util.regex.Pattern
+import java.io.FileInputStream
+import java.io.ObjectInputStream
+import java.io.File
 
-object Query {
-  type REG = ExtractionGroup[ReVerbExtraction]
-
-  val paths = Seq("/scratch/common/openie-demo/test-index",
-    "/scratch2/common/openie-demo/test-index",
-    "/scratch3/common/openie-demo/test-index",
-    "/scratch4/common/openie-demo/test-index")
-  val fetcher = new ParallelExtractionGroupFetcher(paths)
-}
 
 case class Query(
   arg1: Option[String],
@@ -38,9 +32,9 @@ case class Query(
 
   def execute() = {
     def part(eg: REG, part: Symbol) = part match {
-      case 'rel => GroupTitlePart(eg.relNorm, eg.instances.map(_.extraction.source.getRelation.getText).toSeq, None, Seq.empty)
-      case 'arg1 => GroupTitlePart(eg.arg1Norm, eg.instances.map(_.extraction.source.getArgument1.getText).toSeq, eg.arg1Entity, eg.arg1Types)
-      case 'arg2 => GroupTitlePart(eg.arg2Norm, eg.instances.map(_.extraction.source.getArgument2.getText).toSeq, eg.arg2Entity, eg.arg2Types)
+      case 'rel => GroupTitlePart(eg.relNorm, eg.instances.iterator.map(_.extraction.source.getRelation.getText).map(clean).toSeq, None, Seq.empty)
+      case 'arg1 => GroupTitlePart(eg.arg1Norm, eg.instances.iterator.map(_.extraction.source.getArgument1.getText).map(clean).toSeq, eg.arg1Entity, eg.arg1Types)
+      case 'arg2 => GroupTitlePart(eg.arg2Norm, eg.instances.iterator.map(_.extraction.source.getArgument2.getText).map(clean).toSeq, eg.arg2Entity, eg.arg2Types)
     }
 
     def group: REG=>GroupTitle = (this.arg1, this.rel, this.arg2) match {
@@ -57,39 +51,77 @@ case class Query(
     }
 
     val results = Query.fetcher.getGroups(this.arg1, this.rel, this.arg2).map { reg =>
-      reg.copy(instances = reg.instances filter filterInstances)
-    }
-    val groups = Group.fromExtractionGroups(results, group)
+      reg.copy(
+          instances = reg.instances filter filterInstances,
+          arg1Norm = clean(reg.arg1Norm),
+          relNorm = clean(reg.relNorm),
+          arg2Norm = clean(reg.arg2Norm))
+    } filter filterGroups
+    val groups = Group.fromExtractionGroups(results, group).filter(!_.title.text.trim.isEmpty)
 
     groups
   }
+}
 
+object Query {
+  type REG = ExtractionGroup[ReVerbExtraction]
+
+  val paths = Seq("/scratch/common/openie-demo/test-index",
+    "/scratch2/common/openie-demo/test-index",
+    "/scratch3/common/openie-demo/test-index",
+    "/scratch4/common/openie-demo/test-index")
+  val fetcher = new ParallelExtractionGroupFetcher(paths)
 
   private final val pronouns: Set[String] = Set("he", "she", "they", "them",
-     "that", "this", "who", "whom", "i", "you", "him", "her", "we",
-     "it", "the", "a", "an")
+   "that", "this", "who", "whom", "i", "you", "him", "her", "we",
+   "it", "the", "a", "an")
   private final val nonQuestionableChars = Pattern.compile("[\\p{Lower}\\p{Digit} ]+")
-  private final val stripExtraWS = Pattern.compile(" +")
-  private final val stripChars= Pattern.compile("[^\\p{Graph} ]+")
-  private final val leadingBadChars = Pattern.compile("^\\s*(\\\"|\\'|\\()\\s")
-  private final val leadingArticle = Pattern.compile("^\\s*(the|this|these|those|that|a|an)\\s", Pattern.CASE_INSENSITIVE)
+  private final val stripExtraWS = Pattern.compile("\\s+")
+  private final val stripChars= Pattern.compile("[^\\p{Graph}\\p{Cntrl} ]+")
+  private final val leadingBadChars = Pattern.compile("^\\s*(\\.|,|\\\"|\\'|\\()\\s")
+  private final val leadingArticle = Pattern.compile("^\\s*(the|this|these|those|that|a|an)\\s*", Pattern.CASE_INSENSITIVE)
   private final val startCap = Pattern.compile(".*\\b[A-Z].*")
   private final val likelyError = Pattern.compile(".*(http|\\(|\\)|\\\"|\\[|thing).*", Pattern.CASE_INSENSITIVE)
+
+  def fromFile(file: File) = {
+    using (new FileInputStream(file)) { fis =>
+      using (new ObjectInputStream(fis) {
+              override def resolveClass(desc: java.io.ObjectStreamClass): Class[_] = {
+                try { Class.forName(desc.getName, false, getClass.getClassLoader) }
+                catch { case ex: ClassNotFoundException => super.resolveClass(desc) }
+              }}) { in =>
+        in.readObject().asInstanceOf[Iterable[ExtractionGroup[ReVerbExtraction]]]
+      }
+    }
+  }
+
+  def clean(string: String) = {
+    var clean = string.trim
+
+    clean = stripChars.matcher(clean).replaceAll("");
+    clean = stripExtraWS.matcher(clean).replaceAll(" ").trim();
+    clean = leadingBadChars.matcher(clean).replaceAll("");
+
+    clean
+  }
+
   def filterInstances(inst: Instance[ReVerbExtraction]): Boolean = {
     def clean(arg: String) = {
-      var clean = arg
+      var clean = this.clean(arg.trim)
 
-      clean = stripChars.matcher(clean).replaceAll("");
-      clean = stripExtraWS.matcher(clean).replaceAll(" ").trim();
-      clean = leadingBadChars.matcher(clean).replaceAll("");
       clean = leadingArticle.matcher(clean).replaceAll("");
 
       clean.toLowerCase
     }
 
+    def tooShort(part: String) = {
+      part.size - nonQuestionableChars.matcher(part).replaceAll("").size <= 2
+    }
+
     val arg1clean = clean(inst.extraction.source.getArgument1.getTokensAsString)
     val arg2clean = clean(inst.extraction.source.getArgument2.getTokensAsString)
     val relclean = clean(inst.extraction.source.getRelation.getTokensAsString)
+    val extr = arg1clean + relclean + arg2clean
 
     if (arg1clean.length + relclean.length + arg2clean.length > 120) {
       false
@@ -98,6 +130,27 @@ case class Query(
       false
     }
     else if (arg1clean.isEmpty || relclean.isEmpty || arg2clean.isEmpty) {
+      false
+    }
+    else if (arg1clean == arg2clean) {
+      false
+    }
+    else if (nonQuestionableChars.matcher(extr).replaceAll("").size >= 5) {
+      false
+    }
+    else if (tooShort(arg1clean) || tooShort(relclean) || tooShort(arg2clean)) {
+      false
+    }
+    else if (likelyError.matcher(arg1clean).matches() || likelyError.matcher(arg2clean).matches()) {
+      false
+    }
+    else {
+      true
+    }
+  }
+
+  def filterGroups(group: ExtractionGroup[ReVerbExtraction]): Boolean = {
+    if (group.arg1Norm.trim.isEmpty || group.relNorm.trim.isEmpty || group.arg2Norm.trim.isEmpty) {
       false
     }
     else {
