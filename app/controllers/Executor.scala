8f7f86af7b698bf2b580c61fcfdc8819e1c3c434
Michael Schmitz <michael@schmitztech.com>
2012-11-27 12:35:31 -0800

    Work around for relation filter efficiency.

--- app/controllers/Executor.scala
+++ app/controllers/Executor.scala
@@ -173,18 +173,18 @@ object Executor {
         true
       }
     }
-    
-    
+
+
 
     def filterCorpora(instance: Instance[_ <: Extraction]) = query.corpora match {
       case Some(CorporaConstraint(corporaString)) => corporaString.contains(instance.corpus)
       case _ => true
     }
 
-    def filterRelation(spec: QuerySpec)(group: ExtractionGroup[ReVerbExtraction]) = spec.relNorm match {
+    def filterRelation(relNorm: Option[String])(group: ExtractionGroup[ReVerbExtraction]) = relNorm match {
       // if the query does not constrain rel, we can ignore this filter
       case Some(queryRelNorm) => {
-        val filteredRelNormTokens = queryRelNorm.toLowerCase.split(" ").filter { str => !prepositions.contains(str) } toSet;
+        val filteredRelNormTokens = (queryRelNorm.toLowerCase.split(" ").filter { str => !prepositions.contains(str) }).toSet
         if (!filteredRelNormTokens.isEmpty) filterRelationHelper(filteredRelNormTokens, group) else true
       }
       case None => true
@@ -205,12 +205,12 @@ object Executor {
       }
     }
 
-    
-    
+
+
     def filterArg2DayOfWeek(group: ExtractionGroup[_ <: Extraction]): Boolean = {
       !daysOfWeek.contains(group.arg2.norm)
     }
-    
+
     def filterGroups(spec: QuerySpec)(group: ExtractionGroup[_ <: Extraction]): Boolean = {
       // if there are constraints, apply them to each part in case lucene messed up
       def filterPart(constraint: Option[Constraint], entity: Option[FreeBaseEntity], types: Iterable[FreeBaseType]) = {
@@ -303,7 +303,7 @@ object Executor {
             arg1 = ExtractionArgument(Query.clean(reg.arg1.norm), arg1Entity, arg1Types),
             rel = reg.rel.copy(norm = Query.clean(reg.rel.norm)),
             arg2 = ExtractionArgument(Query.clean(reg.arg2.norm), arg2Entity, arg2Types))
-        } filter filterGroups(spec) filter filterRelation(spec) filter (_.instances.size > 0) filter filterArg2DayOfWeek toList
+        } filter filterGroups(spec) filter filterRelation(spec.relNorm) filter (_.instances.size > 0) filter filterArg2DayOfWeek toList
       }
 
     Logger.debug(spec.toString + " filtered with " + filtered.size + " answers in " + Timing.Seconds.format(nsFiltered))
@@ -321,5 +321,5 @@ object Executor {
   private final val likelyErrorPattern = Pattern.compile(".*(http|\\(|\\)|\\\"|\\[|thing).*", Pattern.CASE_INSENSITIVE)
 
   private final val daysOfWeek = Set("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "mon.", "tues.", "wed.", "thurs.", "fri.", "sat.", "sun.", "thurs", "tues", "mon", "last week")
-  
+
 }
