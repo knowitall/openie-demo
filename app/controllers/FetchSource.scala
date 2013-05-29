37ba6a8d0d3b642e2d4c23425781c726aa2115fb
Michael Schmitz <michael@schmitztech.com>
2013-05-29 10:36:11 -0700

    Cleaned up SolrSource code.

--- app/controllers/FetchSource.scala
+++ app/controllers/FetchSource.scala
@@ -44,8 +44,11 @@ object Fetch {
 
 case object SolrSource extends FetchSource {
   import edu.knowitall.openie.models.serialize.Chill
+
   val kryo = Chill.createInjection()
   val solr = new HttpSolrServer(current.configuration.getString("source.solr.url").get)
+
+  // SOLR settings
   solr.setSoTimeout(20000); // socket read timeout
   solr.setConnectionTimeout(20000);
   solr.setDefaultMaxConnectionsPerHost(100);
@@ -57,6 +60,9 @@ case object SolrSource extends FetchSource {
   override def fetch(spec: QuerySpec) = {
     val squery = new SolrQuery()
 
+    // turn a string into a query string.  We don't use a normalized field in SOLR
+    // because extractions are grouped by lemmas--the actual text might be unique
+    // between the instances--which one to use?
     def normalize(string: String) = {
       val tokenized = Fetch.tokenizer.synchronized {
         Fetch.tokenizer.tokenize(string)
@@ -65,20 +71,35 @@ case object SolrSource extends FetchSource {
       (tokenized.map(_.string) map MorphaStemmer.lemmatize).mkString(" ")
     }
 
-    val parts = (Iterable("arg1", "rel", "arg2", "arg1_types", "arg2_types", "arg1_entity", "arg2_entity") zip Iterable(spec.arg1 map normalize, spec.rel map normalize, spec.arg2 map normalize, spec.arg1Types, spec.arg2Types, spec.arg1Entity, spec.arg2Entity)).flatMap {
+    // figure which fields we will query for
+    // (fieldName, fieldValue)
+    val parts = (Iterable(
+        "arg1", "rel", "arg2",
+        "arg1_types", "arg2_types",
+        "arg1_entity", "arg2_entity") zip Iterable(
+            spec.arg1 map normalize, spec.rel map normalize, spec.arg2 map normalize,
+            spec.arg1Types, spec.arg2Types,
+            spec.arg1Entity, spec.arg2Entity)).flatMap {
       case (a, b) => if (b.isEmpty) {
         None
       } else {
         Some("+" + a, b.get)
       }
     }
+
+    // build the query text
     val queryText = parts.map { case (field, value) => field + ":\"" + value + "\"" }.mkString(" ")
 
     Logger.debug("SOLR query: " + queryText)
+
+    // set query options
     squery.setQuery(queryText + " size:[50 TO *]")
     squery.setSort("size", SolrQuery.ORDER.desc)
     squery.setRows(1000)
     squery.setTimeAllowed(Executor.queryTimeout)
+
+    // submit query and await response
+    // TODO: make async
     val response = try {
       Timing.timeThen {
         solr.query(squery)
@@ -90,46 +111,41 @@ case object SolrSource extends FetchSource {
     import scala.collection.JavaConverters._
     val groups =
       Timing.timeThen {
+        // convert the results to ExtractionGroup[ReVerbExtraction]
         for (result <- response.getResults().asScala) yield {
+          // deserialize instances
           val bytes = result.getFieldValue("instances").asInstanceOf[Array[Byte]]
           val instances: List[Instance[ReVerbExtraction]] =
             kryo.invert(bytes)
               .getOrElse(throw new IllegalArgumentException("Could not deserialize instances: " + bytes.toSeq.toString))
               .asInstanceOf[List[Instance[ReVerbExtraction]]]
 
-          val arg1 = ExtractionArgument(
-            norm = result.getFieldValue("arg1_norm").asInstanceOf[String],
-            entity = {
-              if (!result.containsKey("arg1_entity_id")) None
-              else {
-                val id = result.getFieldValue("arg1_entity_id").asInstanceOf[String]
-                val name = result.getFieldValue("arg1_entity_name").asInstanceOf[String]
-                val inlink_ratio = result.getFieldValue("arg1_entity_inlink_ratio").asInstanceOf[Double]
-                val score = result.getFieldValue("arg1_entity_score").asInstanceOf[Double]
-                Some(FreeBaseEntity(name, id, score, inlink_ratio))
-              }
-            },
-            types =
-              if (!result.containsKey("arg1_fulltypes")) Set.empty
-              else result.getFieldValue("arg1_fulltypes").asInstanceOf[java.util.List[String]].asScala.map(FreeBaseType.parse(_).get).toSet)
+          def buildArgument(argName: String) = {
+            ExtractionArgument(
+              norm = result.getFieldValue(argName).asInstanceOf[String],
+              entity = {
+                // if there is an entity id, we have an entity
+                if (!result.containsKey(argName + "_entity_id")) None
+                else {
+                  val id = result.getFieldValue(argName + "_entity_id").asInstanceOf[String]
+                  val name = result.getFieldValue(argName + "_entity_name").asInstanceOf[String]
+                  val inlink_ratio = result.getFieldValue(argName + "_entity_inlink_ratio").asInstanceOf[Double]
+                  val score = result.getFieldValue(argName + "_entity_score").asInstanceOf[Double]
+                  Some(FreeBaseEntity(name, id, score, inlink_ratio))
+                }
+              },
+              types =
+                // if the set "fulltypes" is non-empty, we have types
+                if (!result.containsKey(argName + "_fulltypes")) Set.empty
+                else {
+                  val types = result.getFieldValue(argName + "_fulltypes").asInstanceOf[java.util.List[String]].asScala
+                  types.map(FreeBaseType.parse(_).get).toSet
+                })
+          }
 
-          val arg2 = ExtractionArgument(
-            norm = result.getFieldValue("arg2_norm").asInstanceOf[String],
-            entity = {
-              if (!result.containsKey("arg2_entity_id")) None
-              else {
-                val id = result.getFieldValue("arg2_entity_id").asInstanceOf[String]
-                val name = result.getFieldValue("arg2_entity_name").asInstanceOf[String]
-                val inlink_ratio = result.getFieldValue("arg2_entity_inlink_ratio").asInstanceOf[Double]
-                val score = result.getFieldValue("arg2_entity_score").asInstanceOf[Double]
-                Some(FreeBaseEntity(name, id, score, inlink_ratio))
-              }
-            },
-            types =
-              if (!result.containsKey("arg2_fulltypes")) Set.empty
-              else result.getFieldValue("arg2_fulltypes").asInstanceOf[java.util.List[String]].asScala.map(FreeBaseType.parse(_).get).toSet)
-
-          val rel = ExtractionRelation(result.getFieldValue("rel_norm").asInstanceOf[String])
+          val rel = ExtractionRelation(result.getFieldValue("rel").asInstanceOf[String])
+          val arg1 = buildArgument("arg1")
+          val arg2 = buildArgument("arg2")
 
           ExtractionGroup[ReVerbExtraction](
             arg1 = arg1,
