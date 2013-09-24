8669782a1781fd505a700fe308d2a6f67237419a
Rob Bart <rbart@cs.washington.edu>
2013-09-24 12:15:34 -0700

    Remove openie-backend dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,6 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "edu.washington.cs.knowitall.openie" %% "openie-backend" % "1.1-SNAPSHOT",
-      "edu.washington.cs.knowitall.openie" %% "openie-models" % "1.1-SNAPSHOT",
       "edu.knowitall.triplestore-qa" %% "triplestore-qa" % "0.1-SNAPSHOT",
       "edu.washington.cs.knowitall.nlptools" %% "nlptools-stem-morpha" % "2.4.2",
       "edu.washington.cs.knowitall.common-scala" %% "common-scala" % "1.1.2-SNAPSHOT",
