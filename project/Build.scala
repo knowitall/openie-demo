ee799cfc933b99a87dc399dedfea60d64c2ed696
Rob Bart <rbart@cs.washington.edu>
2013-10-24 12:53:57 -0700

    Switch from common-scala-1.1.2-SNAPSHOT to common-scala-1.1.2

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       "edu.knowitall.triplestore-qa" %% "triplestore-qa" % "0.1-SNAPSHOT",
       "edu.washington.cs.knowitall.nlptools" %% "nlptools-stem-morpha" % "2.4.2",
-      "edu.washington.cs.knowitall.common-scala" %% "common-scala" % "1.1.2-SNAPSHOT",
+      "edu.washington.cs.knowitall.common-scala" %% "common-scala" % "1.1.2",
       "net.debasishg" %% "sjson" % "0.19",
       "org.apache.solr" % "solr-solrj" % "4.4.0"
     )
